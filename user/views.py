39e6146c9e84b65d67b1a36bbdcba053f5fb22c8
LenaFedchenko <lenafedcenko98@gmail.com>
2026-04-30 20:35:50 +0300

    added hash for password, added css

--- user/views.py
+++ user/views.py
@@ -1,20 +1,23 @@
 import flask
+import werkzeug.security as security
 from .model import User
 from project.db import DATABASE
+
 
 def render_register():
 
     if flask.request.method == "POST":
         email = flask.request.form["email"]
         password = flask.request.form["password"]
-    
         if email and password:
-
-            user = User(
-                email = email,
-                password = password
-            )
-            DATABASE.session.add(user)
-            DATABASE.session.commit()
+            email_user = User.query.filter_by(email=email).first()
+            if email_user == None:
+                hashed_password = security.generate_password_hash(password)
+                user = User(
+                    email = email,
+                    password = hashed_password
+                )
+                DATABASE.session.add(user)
+                DATABASE.session.commit()
 
     return flask.render_template("register.html")
